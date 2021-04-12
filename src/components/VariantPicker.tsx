ba119a2a45d09a493406b612cc9c5e69658fdc81
Jamie Barton <jamie@notrab.dev>
2021-04-12 22:03:23 +0100

    reset select style

--- src/components/VariantPicker.tsx
+++ src/components/VariantPicker.tsx
@@ -5,7 +5,7 @@ const VariantPicker = ({ variants, ...props }) => {
   return (
     <select
       {...props}
-      className="w-full relative mb-3 sm:mb-0 flex-grow sm:mr-3 pl-3 py-2 bg-white border border-gray-300 focus:border-gray-500 shadow-sm text-gray-500 text-sm focus:outline-none focus:text-gray-900 rounded"
+      className="appearance-none w-full relative mb-3 sm:mb-0 flex-grow sm:mr-3 pl-3 py-2 bg-white border border-gray-300 focus:border-gray-500 shadow-sm text-gray-500 text-sm focus:outline-none focus:text-gray-900 rounded"
     >
       {variants.map(({ external_id, name }) => (
         <option key={external_id} value={external_id}>
