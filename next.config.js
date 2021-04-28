7936f3e10028e931c4a6925324113bb6c95cacb2
Jamie Barton <jamie@notrab.dev>
2021-04-28 17:14:46 +0100

    update

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const withMDX = require("@next/mdx")({
 
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  future: {
-    webpack5: true,
-  },
   images: {
     domains: ["files.cdn.printful.com"],
   },
