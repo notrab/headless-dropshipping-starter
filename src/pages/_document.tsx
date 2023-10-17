9423a1af41327e86e81119411492c42790fc2872
Juan Fernandes <hello@juanfernandes.uk>
2023-10-17 14:48:30 +0100

    Add missing favicon and add reference to _document.tsx (#121)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="/favicon.ico"
+            rel="shortcut icon"
+          />
         </Head>
         <body className="antialiased">
           <Main />
